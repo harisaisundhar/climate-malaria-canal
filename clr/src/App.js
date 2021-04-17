c11f94aad5b15f2941eb9e4a45ff9566fd335828
Harisai <harisaipravin@gmail.com>
2021-04-17 16:04:16 +0530

    Table changes done

--- clr/src/App.js
+++ clr/src/App.js
@@ -16,24 +16,20 @@ function App() {
       <div className="flex flex-wrap -m-4">
         <div className="xl:w-1/4 md:w-1/2 p-4">
           <Flippy className="bg-gray-100 p-6 rounded-lg">
-          
-                  <FrontSide
-      style={{
-        backgroundColor: '#41669d',
-      }}
-    >
-      <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content"></img>
-            <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-            <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
+            <FrontSide
+              style={{backgroundColor: '#41669d'}}>
+              <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content"></img>
+              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
               <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-    </FrontSide>
-    <BackSide
-      style={{ backgroundColor: '#175852'}}>
-        <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content"></img>
-            <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-            <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
+            </FrontSide>
+            <BackSide
+              style={{ backgroundColor: '#175852'}}>
+              <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content"></img>
+              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
               <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-    </BackSide>
+            </BackSide>
           </Flippy>
         </div>
         <div className="xl:w-1/4 md:w-1/2 p-4">
