78c4ebfba78c2a4a1f42e6799968d57750b121af
Harisai <harisaipravin@gmail.com>
2021-04-18 19:19:50 +0530

    Thanks Ref

--- clr/src/References.js
+++ clr/src/References.js
@@ -1,8 +1,92 @@
+import { Link } from 'react-router-dom';
+
 function References() {
   return (
 <section class="text-gray-600 body-font">
-          <br></br>
-           <h1 class="font-medium title-font text-gray-900 mb-1 text-xl text-center">References</h1><hr></hr>
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">References</h1>
+      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">The reference research papers proved very pivotal for our findings</p>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-200 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2"><a href="https://www.indiawaterportal.org/sites/default/files/iwp2/relative_roles_of_weather_variables_in_malaria_incidence_prashant_goswami_et_al_plos_one_2014.pdf"></a>Comparison over Different
+States of India</h2>
+          <p class="leading-relaxed text-base">Prashant Goswami1
+*, Upadhayula Suryanarayana Murty2
+, Srinivasa Rao Mutheneni2
+,
+Swathi Trithala Krishnan1</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-200 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <circle cx="6" cy="6" r="3"></circle>
+              <circle cx="6" cy="18" r="3"></circle>
+              <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2"><a href="https://www.researchgate.net/publication/49790928_Impacts_of_Climate_Change_on_Public_Health_in_India_Future_Research_Directions">Impacts of Climate Change on Public Health in India</a></h2>
+          <p class="leading-relaxed text-base">Kathleen F. Bush,1 George Luber,2 S. Rani Kotha,3 R.S. Dhaliwal,4 Vikas Kapil,2 Mercedes Pascual,5</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-200 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2"><a href="https://www.researchgate.net/publication/348265401_Prediction_of_Malaria_Incidence_using_Climate_Variability_and_Machine_Learning">Prediction of malaria incidence using climate variability</a></h2>
+          <p class="leading-relaxed text-base">Odu Nkiruka, Rajesh Prasad *, Onime Clement</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-200 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2"><a href="https://health2016.globalchange.gov/">THE IMPACTS OF CLIMATE CHANGE ON HUMAN HEALTH IN THE UNITED STATES</a></h2>
+          <p class="leading-relaxed text-base">Allison Crimmins,Michelle Hawkins</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-200 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2"><a href="https://nvbdcp.gov.in/">ANNUAL REPORT 2014-15</a></h2>
+          <p class="leading-relaxed text-base">NATIONAL VECTOR BORNE DISEASE CONTROL PROGRAMME
+</p>
+        </div>
+      </div>
+      <div class="xl:w-1/3 md:w-1/2 p-4">
+        <div class="border border-gray-200 p-6 rounded-lg">
+          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+              <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+            </svg>
+          </div>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-2"><a href="https://www.researchgate.net/publication/291084731_Malaria_Outbreak_Prediction_Model_Using_Machine_Learning">Malaria Outbreak Prediction Model Using Machine Learning</a></h2>
+          <p class="leading-relaxed text-base">Vijeta Sharma</p>
+        </div>
+      </div>
+    </div>
+    <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"><Link to ='/'>Done</Link></button>
+  </div>
 </section>
   );
 }
